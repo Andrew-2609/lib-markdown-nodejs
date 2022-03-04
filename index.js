d620a3ef77e96445545e19581d8e15c7829ad778
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-03-04 17:37:46 -0300

    Feat: implement an error handling in index.js

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
 const chalk = require('chalk');
 const fs = require('fs');
 
+function tratarErro(erro) {
+    throw new Error(chalk.red(
+        erro.code,
+        `Erro ao ler arquivo! Você passou o caminho do ${chalk.bold('arquivo')} corretamente?`
+    ));
+}
+
 function pegarArquivo(caminhoDoArquivo) {
     const encoding = 'utf-8';
-    fs.readFile(caminhoDoArquivo, encoding, (_, texto) => {
+    fs.readFile(caminhoDoArquivo, encoding, (erro, texto) => {
+        if (erro) {
+            tratarErro(erro);
+        }
+
         console.log(chalk.green(texto));
     });
 }
 
 pegarArquivo('./arquivos/texto1.md');
+console.log('Simulando erro:');
+pegarArquivo('./arquivos/');
