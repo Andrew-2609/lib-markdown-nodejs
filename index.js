2cb732e7ae4fe887703a7f1636547a05a9669fc6
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-03-05 10:57:12 -0300

    Feat: implement function extrairLinksDoTexto() using regex along with pegarTexto() previously implemented

--- index.js
+++ index.js
@@ -8,49 +8,26 @@ function tratarErro(erro) {
     ));
 }
 
-function pegarArquivoSincrona(caminhoDoArquivo) {
-    const encoding = 'utf-8';
-    fs.readFile(caminhoDoArquivo, encoding, (erro, texto) => {
-        if (erro) {
-            tratarErro(erro);
-        }
+function extrairLinksDoTexto(texto) {
+    const regex = /\[([^\]]*)\]\((https?:\/\/[^$#\s].[^\s]*)\)/gm;
+    const resultados = []
+    let temp;
 
-        console.log(chalk.green(texto));
-    });
+    while ((temp = regex.exec(texto)) !== null) {
+        resultados.push({[temp[1]]: temp[2]})
+    }
+
+    return resultados;
 }
 
-function pegarArquivoAssincrona(caminhoDoArquivo) {
-    const encoding = 'utf-8';
-    fs.promises.readFile(caminhoDoArquivo, encoding)
-        .then((texto) => console.log(chalk.yellow(texto)))
-        .catch((erro) => tratarErro(erro))
-    ;
-}
-
-async function pegarArquivoAsyncAwait(caminhoDoArquivo) {
+async function pegarTexto(caminhoDoArquivo) {
     const encoding = 'utf-8';
     try {
         const texto = await fs.promises.readFile(caminhoDoArquivo, encoding);
-        console.log(chalk.bgGreen(texto))
+        console.log(extrairLinksDoTexto(texto));
     } catch (erro) {
-        console.log(chalk.bold('Simulando erro:'))
         tratarErro(erro);
     }
 }
 
-/*
-// Testes com função Síncrona
-pegarArquivoSincrona('./arquivos/texto1.md');
-console.log('Simulando erro:');
-pegarArquivoSincrona('./arquivos/');
-
-// Testes com função assíncrona usando .then().catch()
-pegarArquivoAssincrona('./arquivos/texto1.md');
-console.log('Simulando erro:');
-pegarArquivoAssincrona('./arquivos/');*/
-
-// Testes com função assíncrona usando async/await
-// noinspection JSIgnoredPromiseFromCall para o WebStorm
-pegarArquivoAsyncAwait('./arquivos/texto1.md');
-// noinspection JSIgnoredPromiseFromCall para o WebStorm
-pegarArquivoAsyncAwait('./arquivos/');
+pegarTexto('./arquivos/texto1.md');
