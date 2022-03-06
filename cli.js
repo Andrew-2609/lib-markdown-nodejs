caaa273b70887ec8e6cb3dbc7afe12bb444ae571
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-03-06 00:31:14 -0300

    Feat: add call to validarUrls method along with new logic for return statements in cli.js

--- cli.js
+++ cli.js
@@ -1,11 +1,19 @@
 const chalk = require('chalk')
 const {pegarTexto} = require('./index')
+const validarUrls = require('./http-validacao')
 
-const caminho = process.argv[process.argv.length - 1];
+const caminho = process.argv;
 
 async function processarTexto(caminhoDeArquivo) {
-    const resultado = await pegarTexto(caminhoDeArquivo);
-    return chalk.yellow('Lista de links: ') + resultado;
+    const resultado = await pegarTexto(caminhoDeArquivo[2]);
+
+    if (caminhoDeArquivo[3] === 'validar') {
+        console.log(chalk.blue('Links validados: '));
+        return validarUrls(resultado);
+    }
+
+    console.log(chalk.yellow('Lista de links:'));
+    return resultado;
 }
 
 processarTexto(caminho).then(res => console.log(res));
