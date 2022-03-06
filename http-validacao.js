41834b8d857e5425e9fb484d17460152eeb17e37
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-03-06 00:44:10 -0300

    Feat: validate the status of each url in the array in http-validacao.js

--- http-validacao.js
+++ http-validacao.js
@@ -1,9 +1,21 @@
+const fetch = require('node-fetch')
+
+async function checarStatus(arrayUrls) {
+    return await Promise.all(
+        arrayUrls.map(async (url) => {
+            const res = await fetch(url);
+            return res.status;
+        })
+    );
+}
+
 function gerarArrayDeUrls(arrayLinks) {
     return arrayLinks.map(objetoLink => Object.values(objetoLink).join());
 }
 
-function validarUrls(arrayLinks) {
-    return gerarArrayDeUrls(arrayLinks);
+async function validarUrls(arrayLinks) {
+    const arrayDeUrls = gerarArrayDeUrls(arrayLinks);
+    return await checarStatus(arrayDeUrls);
 }
 
 module.exports = validarUrls;
