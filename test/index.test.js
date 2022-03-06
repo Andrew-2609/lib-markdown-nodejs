62acfc57c86768f9a77b2a2e900d47d8264c97f9
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-03-06 14:46:02 -0300

    Test: implement test 'deve lançar um erro para caminho de arquivo inválido' for pegarTexto() method of index.js

--- test/index.test.js
+++ test/index.test.js
@@ -1,10 +1,13 @@
 const {pegarTexto} = require('../index');
+const chalk = require('chalk');
 
 const resultados = [
     {
         FileList: `https://developer.mozilla.org/pt-BR/docs/Web/API/FileList`
     }
-]
+];
+
+const mensagemDeErroComChalk = chalk.red(`ENOENT Erro ao ler arquivo! Você passou o caminho do ${chalk.bold('arquivo')} corretamente?`);
 
 describe('pegarTexto::', () => {
     it('deve ser uma função', () => {
@@ -23,5 +26,10 @@ describe('pegarTexto::', () => {
     it('deve retornar mensagem para texto sem links', async () => {
         const resultado = await pegarTexto('./test/arquivos/texto1-sem-links.md');
         expect(resultado).toBe('Não há links.');
-    })
+    });
+
+    it('deve lançar um erro para caminho de arquivo inválido', async () => {
+        await expect(pegarTexto('./test/arquivos/texto99.md')).rejects
+            .toThrow(mensagemDeErroComChalk);
+    });
 });
