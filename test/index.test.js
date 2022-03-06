d7bd27a9997eb340ef078acbcdf97bb5cd45ba47
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-03-06 13:42:37 -0300

    Test: implement test 'deve retornar um array com resultados' for pegarTexto() function of index.js

--- test/index.test.js
+++ test/index.test.js
@@ -1,7 +1,18 @@
 const {pegarTexto} = require('../index');
 
+const resultados = [
+    {
+        FileList: `https://developer.mozilla.org/pt-BR/docs/Web/API/FileList`
+    }
+]
+
 describe('pegarTexto::', () => {
     it('deve ser uma função', () => {
         expect(typeof pegarTexto).toBe('function');
-    })
+    });
+
+    it('deve retornar um array com resultados', async () => {
+        const resultado = await pegarTexto('./test/arquivos/texto1.md');
+        expect(resultado).toEqual(resultados);
+    });
 });
